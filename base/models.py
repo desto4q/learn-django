e3ba2b52e17b193e14ed62f87f25039806f0d83a
destiny <desto4q@gmail.com>
2023-06-03 03:06:04 +0100

    progress

--- base/models.py
+++ base/models.py
@@ -11,14 +11,15 @@ class Topic(models.Model):
 
 
 class Room(models.Model):
-    # host
-    Topic = models.ForeignKey(Topic,on_delete=models.SET_NULL, null=True)
+    host = models.ForeignKey(User,on_delete=models.SET_NULL, null=True)
+    topic = models.ForeignKey(Topic,on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=200)
     description = models.TextField(null=True, blank=True)
     # participant = models
     updated = models.DateTimeField(auto_now=True)
-    creared = models.DateTimeField(auto_now_add=True)
-
+    created = models.DateTimeField(auto_now_add=True)
+    class Meta:
+        ordering = ["-updated","-created"]
     def __str__(self) -> str:
         return self.name
     
@@ -28,7 +29,7 @@ class message(models.Model):
     room = models.ForeignKey(Room, on_delete=models.CASCADE)
     body = models.TextField(null=False,blank=False)
     updated = models.DateTimeField(auto_now=True)
-    creared = models.DateTimeField(auto_now_add=True)
+    created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self) -> str:
-        return self.body[50]
+        return self.body[0:50]
